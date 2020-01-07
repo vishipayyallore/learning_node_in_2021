4cf0457723bb13ed26f44753515d3bd8793c7010
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-01-07 22:16:55 +0530

    Basic Middleware

--- first_webapi/app.js
+++ first_webapi/app.js
@@ -2,17 +2,25 @@ const express = require('express')
 
 const app = express();
 
+var logger = function (req, res, next) {
+    console.log(`Middleware will be always execute when user reaches the /professors route ${new Date().toISOString()}`);
+    next();
+}
+
+// Middleware (Executes the function)
+app.use('/professors', (logger));
+
 // Routes
-app.get('/', (req, res) =>{
-    res.send('Hello NodeJS. This is my first Web API.')
+app.get('/', (req, res) => {
+    res.send('Hello NodeJS. This is my first Web API.');
 });
 
-app.get('/professors', (req, res) =>{
-    res.send('List of Professors.')
+app.get('/professors', (req, res) => {
+    res.send('List of Professors.');
 });
 
-app.get('/students', (req, res) =>{
-    res.send('List of Students.')
+app.get('/students', (req, res) => {
+    res.send('List of Students.');
 });
 
 // Listen to the server
