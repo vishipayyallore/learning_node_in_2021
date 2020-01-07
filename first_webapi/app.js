5c3ddf88f0fb615d483a5f6034beb611521253fd
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-01-07 21:35:35 +0530

    Update app.js

--- first_webapi/app.js
+++ first_webapi/app.js
@@ -7,5 +7,13 @@ app.get('/', (req, res) =>{
     res.send('Hello NodeJS. This is my first Web API.')
 });
 
+app.get('/professors', (req, res) =>{
+    res.send('List of Professors.')
+});
+
+app.get('/students', (req, res) =>{
+    res.send('List of Students.')
+});
+
 // Listen to the server
 app.listen(3000);
