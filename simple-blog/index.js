af9eaea8456280745ac50931146cedbea73a9ba4
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-01-26 17:02:24 +0530

    Simple app

---
+++ simple-blog/index.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const webApplication = http.createServer((request, response) => { 
+    response.statusCode = 200;
+    response.write(`<h1>Hello Node !!!</h1> Request Received at: <a href='${request.url}'>${request.url}</a>`);
+    response.end();
+});
+
+var port = process.env.PORT || 8080;
+webApplication.listen(port);
