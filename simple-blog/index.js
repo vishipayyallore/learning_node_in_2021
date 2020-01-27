e4ed831663595e13649e1135e60fc45e3db6c612
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-01-28 05:11:21 +0530

    Few Changes

--- simple-blog/index.js
+++ simple-blog/index.js
@@ -2,28 +2,31 @@ const http = require('http');
 const fs = require('fs');
 
 const indexFileName = 'home/index.html';
+const contactUsFileName = 'contactUs/contactUs.html';
+const fileNotFound = 'shared/FileNotFound.html';
 
-const webApplication = http.createServer((request, response) => { 
-
-    const homePage = fs.readFileSync(indexFileName)
-
-    //readFileContents('index1.html');
-    fs.readFile(indexFileName, 'utf8', function(err, contents) {
-        console.log(contents);
-    });
-
-    response.statusCode = 200;
-    // response.write(`<h1>Hello Node !!!</h1> Request Received at: <a href='${request.url}'>${request.url}</a>`);
-    response.write(homePage);
-    response.end();
+const webApplication = http.createServer((request, response) => {
+    if (request.url === '/home' || request.url === '/') {
+        readFileContents(indexFileName, response);
+    } else if (request.url === '/contactus') {
+        readFileContents(contactUsFileName, response);
+    } else {
+        readFileContents(fileNotFound, response);
+    }
 });
 
-function readFileContents(fileName){
-    fs.readFile(fileName, 'utf8', function(err, contents) {
-        if(err){
+function readFileContents(fileName, response) {
+    fs.readFile(fileName, 'utf8', function (err, contents) {
+        if (err) {
             console.log(err);
+            response.statusCode = 404;
+            response.write(`${fileName} not found`);
+            response.end();
         } else {
-            console.log(contents);
+            response.statusCode = 200;
+            response.write(contents);
+            response.end();
+            // console.log(contents);
         }
     });
 }
@@ -33,3 +36,23 @@ var port = process.env.PORT || 8080;
 webApplication.listen(port, () => {
     console.log(`Web Application is listening on ${port}. http://localhost:${port}`);
 });
+
+
+
+
+/*
+const homePage = fs.readFileSync(indexFileName)
+
+//readFileContents('index1.html');
+fs.readFile(indexFileName, 'utf8', function(err, contents) {
+    console.log(contents);
+});
+*/
+
+    // const data = readFileContents(indexFileName, response);
+    // console.log(`Output: ${data}`);
+
+    // response.statusCode = 200;
+    // response.write(`<h1>Hello Node !!!</h1> Request Received at: <a href='${request.url}'>${request.url}</a>`);
+    // response.write(homePage);
+    // response.end();
