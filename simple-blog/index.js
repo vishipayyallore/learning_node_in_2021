9d0bf6a7fa227de947a082a7b96453aa3b0885e0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-01-27 22:53:35 +0530

    Few Changes

--- simple-blog/index.js
+++ simple-blog/index.js
@@ -1,10 +1,23 @@
 const http = require('http');
+const fs = require('fs');
 
 const webApplication = http.createServer((request, response) => { 
+
+    fs.readFileSync('./home/index.html', function(error, data) {
+        if(error){
+            console.log(`File Not Found`);
+        } else {
+            console.log(`File Found`);
+        }
+    });
+
+
     response.statusCode = 200;
     response.write(`<h1>Hello Node !!!</h1> Request Received at: <a href='${request.url}'>${request.url}</a>`);
     response.end();
 });
+
+
 
 var port = process.env.PORT || 8080;
 
