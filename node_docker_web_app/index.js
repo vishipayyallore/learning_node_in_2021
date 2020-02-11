9439ef64f870cb8bc4cc7a6f9d75ae5224cc4d92
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-02-11 21:25:00 +0530

    Few Changes

---
+++ node_docker_web_app/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+
+// App
+const app = express();
+
+// Constants
+const PORT = 8080;
+const HOST = '127.0.0.1';
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, HOST, () => {
+    console.log(`Running on http://${HOST}:${PORT}`);
+});
