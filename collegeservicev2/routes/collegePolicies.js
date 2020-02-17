fa1a71fe9ae18b0721dc0532595a76a83f4fdb1b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-02-17 12:56:13 +0530

    ..

---
+++ collegeservicev2/routes/collegePolicies.js
@@ -0,0 +1,3 @@
+'use strict';
+
+const policyDocuments = ['apple', 'strawberry', 'cherry', 'cranberries'];
--- collegeservicev2/routes/students.js
+++ collegeservicev2/routes/students.js
@@ -1,6 +1,12 @@
-const express = require('express')
+'use strict';
 
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const zlib = require('zlib')
 const router = express.Router();
+
+const policyDocuments = ['apple', 'strawberry', 'cherry', 'cranberries'];
 
 router.get('/', (req, res) => {
     res.send('Students default route.');
@@ -8,6 +14,26 @@ router.get('/', (req, res) => {
 
 router.get('/list', (req, res) => {
     res.send('List of Students.');
+});
+
+router.get('/:policyFile', (req, res) => {
+
+    const policyIdentifier = req.params.policyFile;
+
+    fs.readFile(`documents/${policyIdentifier}.txt`, 'utf8', function(err, contents) {
+        if(err){
+            console.error(`Error in reading policy1.txt ${err}`);
+        } else {
+            console.log(contents);
+            res.send(contents);
+        }
+    });
+
+    /*
+    fs.createReadStream(path.join('documents', 'policy1.txt'))
+    .pipe(zlib.createGzip())
+    .pipe(res);
+    */
 });
 
 module.exports = router;
