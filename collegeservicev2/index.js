730742fc8ec64ddecb7f70b5341b287dde0e26ec
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-02-17 09:38:06 +0530

    Initial Version

---
+++ collegeservicev2/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
+require('dotenv/config');
+
+app.use(bodyParser.json());
+
+// Import Routes
+const professorsRoute = require('./routes/professors');
+const studentsRoute = require('./routes/students');
+
+// Middleware (To Import Routes)
+app.use('/professors', professorsRoute);
+app.use('/students', studentsRoute);
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('NodeJS express Web API connecting to MongoDb.');
+});
+
+// Connecting to the MongoDb Cloud Instance
+mongoose.connect(process.env.MongoDbConnection, {
+    useNewUrlParser: true, useUnifiedTopology: true
+}, (error) => {
+    if (error) {
+        console.log(`Error Connecting to Cloud MongoDb ${error}`);
+    } else {
+        console.log('Connected to MongoDb in Cloud');
+    }
+});
+
+var port = process.env.PORT || 3000;
+
+// Listen to the server
+app.listen(port, () => {
+    console.log(`Env Port: ${process.env.PORT}`);
+    console.log(`Server Listening at port ${port}. http://localhost:${port}`);
+});
+
