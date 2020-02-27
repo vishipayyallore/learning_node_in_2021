542444177ba6b98563dd26d2988987d5dcbf1530
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-02-27 22:23:13 +0530

    JSON

--- basics/first-server/main1.js
+++ basics/first-server/main1.js
@@ -1,29 +1,13 @@
 const http = require('http');
 
-const httpHeaders = { 'Content-Type': 'text/plain', 'Cache-Control': 'max-age=180' };
-// const httpHeaders = '{ "Content-Type": "text/plain" }';
+const httpHeaders = { 'Content-Type': 'application/json', 'Cache-Control': 'max-age=180' };
 
 const webApi = http.createServer((request, response) => {
 
     // Sending Http Header, Status, Content Type
     response.writeHead(200, httpHeaders);
 
-    // var headersObject = JSON.parse(httpHeaders);
-    // var lines = headersObject.Files.Lines;
-    // var keys = Object.keys(lines);
-
-    /*
-    keys.array.forEach(element => {
-        response.write(`Keys ${element}`);
-    });
-
-    headersObject.Files.Lines.array.forEach(element => {
-        response.write(`Data ${element}`);
-    });
-    */
-
-    response.write('\nThe Server Application.');
-    response.end();
+    response.end(JSON.stringify(httpHeaders));
 });
 
 const port = process.env.PORT || 8090;
