fc9d4c548794bd87fa62d80663bf4ef156605fc6
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-05-12 23:46:52 +0530

    Initial Version

---
+++ todo-server/src/server.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+import * as express from 'express';
+ 
+const app = express();
+ 
+app.get('/', (request, response) => {
+  response.send('Hello world!');
+});
+ 
+app.listen(5000);
