761a5dd9dc395b8212d0f610c738fb7f30a95a7a
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-05-12 23:58:24 +0530

    Few changes

--- todo-server/src/server.ts
+++ todo-server/src/server.ts
@@ -1,11 +1,23 @@
 'use strict';
 
 import * as express from 'express';
- 
-const app = express();
- 
-app.get('/', (request, response) => {
-  response.send('Hello world!');
+
+const webApi = express();
+
+function loggerMiddleware(request: express.Request, response: express.Response, next) {
+    console.log(`${new Date()} :: ${request.method} ${request.path} ${response.statusCode}`);
+    next();
+}
+
+webApi.use(loggerMiddleware);
+
+webApi.get('/api/default', (request, response) => {
+    response.send('This is Default Route\'s Output.');
 });
- 
-app.listen(5000);
+
+const port = process.env.PORT || 3060;
+
+webApi.listen(port, () => {
+    console.log(`Env Port: ${process.env.PORT}`);
+    console.log(`Server Listening at port ${port}. http://localhost:${port}/api/default`);
+});
