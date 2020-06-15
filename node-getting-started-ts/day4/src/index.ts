fd61edcb2467e8e0e165100a2642ac0e1912c9ec
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-06-15 20:57:08 +0530

    Few Changes

---
+++ node-getting-started-ts/day4/src/index.ts
@@ -0,0 +1,21 @@
+'use strict';
+
+const message = `This is setTimeout's DEFAULT message.`;
+const WaitTimeOneSecond = 1000;
+
+const logMessage = (message: string) => {
+    console.log(message);
+};
+
+// setTimeout Example 1
+setTimeout(() => {
+    logMessage(`Example 1 :: ${message}`);
+}, WaitTimeOneSecond);
+logMessage(`Wait for setTimeout Example 1's output.`);
+
+// setTimeout Example 2
+setTimeout(function () {
+    logMessage(`Example 2 :: ${message}`);
+}, WaitTimeOneSecond);
+logMessage(`Wait for setTimeout Example 2's output.`);
+
