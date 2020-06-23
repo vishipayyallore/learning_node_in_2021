d67bc63b890cf6c9e10fdfc0d07ed5a1bbe09222
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-06-24 00:32:50 +0530

    ..

--- node-getting-started-ts/settimeoutdemos/src/settimeoutdemo1.ts
+++ node-getting-started-ts/settimeoutdemos/src/settimeoutdemo1.ts
@@ -1,23 +1,28 @@
 'use strict';
 
-import * as colors from 'colors';
+// import * as Color from 'colors';
 
 const message = `This is setTimeout's DEFAULT message.`;
 const WaitTimeOneSecond = 1000;
 
 const logMessage = (message: string) => {
+
     console.log(message);
+
 };
 
 // setTimeout Example 1
 setTimeout(() => {
+
     logMessage(`Example 1 :: ${message}`);
+
 }, WaitTimeOneSecond);
 logMessage(`Wait for setTimeout Example 1's output.`);
 
 // setTimeout Example 2
 setTimeout(function () {
+
     logMessage(`Example 2 :: ${message}`);
+
 }, WaitTimeOneSecond);
 logMessage(`Wait for setTimeout Example 2's output.`);
-
