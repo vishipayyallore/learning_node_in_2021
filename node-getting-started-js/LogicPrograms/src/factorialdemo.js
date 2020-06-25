54ff8515d1621711bec0dc6063ef3e4666041f86
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-06-25 07:38:09 +0530

    Feew Changes

---
+++ node-getting-started-js/LogicPrograms/src/factorialdemo.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function factorial(value1) {
+    let results = value1--;
+
+    if (value1 === 0 || value1 === 1) {
+        return 1;
+    }
+
+    while (value1 > 0) {
+        results = results * (value1--);
+    }
+
+    return results;
+}
+
+let value1 = 5;
+const fact = factorial(value1);
+console.log(`Factorial of ${value1} is ${fact}`);
