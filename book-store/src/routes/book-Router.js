b080b731f82c9991526d7b4119cec0935cb6c786
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-07-07 16:06:38 +0530

    Update book-Router.js

--- book-store/src/routes/book-Router.js
+++ book-store/src/routes/book-Router.js
@@ -12,6 +12,31 @@ function routes(Book) {
         .post(bookController.post)
         .get(bookController.get);
 
+    // Middleware For Retrieving the Book
+    bookRouter.use('/books/:bookId', (request, response, next) => {
+        console.log(`Using Middleware for finding Book.`);
+
+        Book.findById(request.params.bookId, (error, book) => {
+            if (error) {
+                return response.status(500).json(error);
+            }
+
+            if (book) {
+                console.log(`Book Found: ${book}`);
+                request.book = book;
+                return next();
+            }
+
+            return response.status(404).json();
+        });
+
+    });
+
+    bookRouter.route('/books/:bookId')
+        .get((request, response) => {
+            response.status(200).json(request.book)
+        });
+
     return bookRouter;
 }
 
