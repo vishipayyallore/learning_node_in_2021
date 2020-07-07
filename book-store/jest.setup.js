c2a9890d6607a062295bb75d5e44567418b7cedf
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-07-08 00:04:50 +0530

    ..

---
+++ book-store/jest.setup.js
@@ -0,0 +1,5 @@
+// By default each test has a default timeout of 5 seconds
+// After 5 seconds jest exits from the test
+
+console.log('Running jest.setup.js')
+jest.setTimeout(30 * 1000); // 30 seconds
