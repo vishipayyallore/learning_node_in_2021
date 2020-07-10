6f70167584aac0b7958d8378d9020063d3f9066b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-07-10 17:30:51 +0530

    Few Changes

--- book-store/tests/unit/book-Router.spec.js
+++ book-store/tests/unit/book-Router.spec.js
@@ -1,28 +1,81 @@
 'use strict';
 
-var sinon = require('sinon');
-var express = require('express');
+const sinon = require('sinon');
+const faker = require('faker')
+const proxyquire = require('proxyquire')
 
 describe('Testing /src/routes/bookRouter.js', () => {
 
+    let expressStub, controllerStub, RouterStub, rootRouteStub, idRouteStub
+
     describe('router', () => {
-        let expressStub, controllerStub, RouterStub, rootRouteStub, idRouterStub
 
+        beforeEach(() => {
+
+            rootRouteStub = {
+                "get": sinon.stub().callsFake(() => rootRouteStub),
+                "post": sinon.stub().callsFake(() => rootRouteStub)
+            };
+
+            idRouteStub = {
+                "get": sinon.stub().callsFake(() => idRouteStub),
+                "use": sinon.stub().callsFake(() => idRouteStub)
+            };
+
+            RouterStub = {
+                route: sinon.stub().callsFake((route) => {
+                    if (route === '/books/:bookId') {
+                        return idRouteStub
+                    }
+                    return rootRouteStub
+                })
+            };
+
+            expressStub = {
+                Router: sinon.stub().returns(RouterStub)
+            };
+
+            controllerStub = {
+                post: sinon.mock(),
+                get: sinon.mock(),
+                getBookById: sinon.mock()
+            };
+
+            proxyquire('../../src/routes/book-Router.js',
+                {
+                    'express': expressStub,
+                    '../controllers/books.Controller.js': controllerStub
+                }
+            );
+
+        });
+
+        test('should map root get() router with controller::get()', () => {
+
+            rootRouteStub.get('/books', controllerStub.get);
+
+            expect(RouterStub.route.calledWith('/books'));
+            expect(rootRouteStub.get.calledWith(controllerStub.get));
+        });
+
+        test('should map root getBookById() router with controller::getBookById()', () => {
+
+            idRouteStub.get('/books/:bookId', controllerStub.getBookById);
+
+            expect(RouterStub.route.calledWith('/books/:bookId'));
+            expect(idRouteStub.get.calledWith(controllerStub.getBookById));
+        });
+
+        test('should map root post() router with controller::post()', () => {
+
+            rootRouteStub.post('/books', controllerStub.post);
+
+            expect(RouterStub.route.calledWith('/books'));
+            expect(rootRouteStub.post.calledWith(controllerStub.post));
+        });
 
     });
 
-    test.skip('Validate Book Routes', () => {
-
-        const routerStub = {
-            route: sinon.stub().returnsThis(),
-            get: sinon.stub().returnsThis(),
-        };
-
-        sinon.stub(express, 'Router').callsFake(() => routerStub);
-        require('../../src/routes/book-Router');
-        // sinon.assert.calledWith(routerStub.route, '/books', {});
-        sinon.assert.calledWith(routerStub.get, sinon.match.func);
-    });
 
 });
 
