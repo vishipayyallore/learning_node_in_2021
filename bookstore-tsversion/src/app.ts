74ee8fa69e41810cfa5cb9cf378c27869c838320
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-08-20 00:01:20 +0530

    Few Changes

--- bookstore-tsversion/src/app.ts
+++ bookstore-tsversion/src/app.ts
@@ -13,5 +13,11 @@ webApi.use(express.json());
 
 // Default Route
 webApi.get('/api', (request, response) => {
+
+    // Log Request
+    console.log(request);
+    const { headers, url, method } = request;
+    console.log(`Headers: ${JSON.stringify(headers)} \nUrl: ${url} \nMethod: ${method}`);
+
     response.status(200).json("Welcome to Books Web API.");
 });
