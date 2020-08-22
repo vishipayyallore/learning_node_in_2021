efda2e6befffeecd04c11c0c2dacfd17af096838
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-08-22 23:03:13 +0530

    Few Changes

--- bookstore-tsversion/src/server.ts
+++ bookstore-tsversion/src/server.ts
@@ -4,10 +4,11 @@ import * as dotenv from 'dotenv';
 import * as path from 'path';
 import { webApi } from './app';
 
-const _dotenv = dotenv.config({ path: path.resolve(__dirname, './config/appConfig.env')});
+// const _dotenv = dotenv.config({ path: path.resolve(__dirname, './config/appConfig.env')});
+const _dotenv = dotenv.config({ path: path.resolve(process.cwd(), 'src/config/appConfig.env')});
 
 // console.log(`DotEnv: ${_dotenv} | CWD: ${path.resolve(process.cwd(), './config/appConfig.env')}`);
-console.log(_dotenv);
+console.log(_dotenv, __dirname, process.cwd());
 
 const port = process.env.PORT || 3060;
 
