ba65fec303b9faef523a39ffac9c89386879fc02
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-08-30 22:24:16 +0530

    Few Changes

--- book-store/src/app.js
+++ book-store/src/app.js
@@ -4,22 +4,25 @@ const express = require('express');
 
 const Book = require('./models/book.Model');
 const bookRouter = require('./routes/book-Router')(Book);
-const logger = require('./middleware/logger');
+const morganLogger = require('./middleware/logger');
+
 
 // Initialized the application
 const webApi = express();
 
+// Logger Middleware
+webApi.use(morganLogger);
+
 // express middleware to handle the json body request
 webApi.use(express.json());
 
-webApi.use(logger);
+// Default Route
+webApi.get('/api', (request, response) => {
+    response.status(200).json("Welcome to Books Web API.");
+});
 
 // Middleware (To Import Additional Routes)
 webApi.use('/api', bookRouter);
 
-// Default Route
-webApi.get('/', (request, response) => {
-    response.status(200).json("Welcome to Books Web API.");
-});
 
 module.exports = webApi;
--- book-store/src/middleware/logger.js
+++ book-store/src/middleware/logger.js
@@ -1,13 +1,30 @@
+'use strict';
+
+const morgan = require('morgan')
+const chalk = require('chalk');
+
+// Logger Middleware
+const morganLogger = morgan(function (tokens, req, res) {
+  return chalk.blue.bold([
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ].join(' '));
+});
+
+module.exports = morganLogger;
+
+/*
 const logger = (req, res, next) => {
 
     console.log(
       `${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`
     );
-    
+
     next();
 
 };
-  
-module.exports = logger;
+*/
 
-  
