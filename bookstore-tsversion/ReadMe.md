8fb4f02a3299f11d9eb594079a7eab87a709c4eb
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-09-01 19:31:04 +0530

    Few Changes

---
+++ bookstore-tsversion/ReadMe.md
@@ -0,0 +1,1 @@
+# This project contains NodeJS + Express + TypeScript
--- bookstore-tsversion/src/server.ts
+++ bookstore-tsversion/src/server.ts
@@ -8,7 +8,8 @@ import { webApi } from './app';
 const _dotenv = dotenv.config({ path: path.resolve(process.cwd(), 'src/config/appConfig.env')});
 
 // console.log(`DotEnv: ${_dotenv} | CWD: ${path.resolve(process.cwd(), './config/appConfig.env')}`);
-console.log(_dotenv, __dirname, process.cwd());
+
+// console.log(_dotenv, __dirname, process.cwd());
 
 const port = process.env.PORT || 3060;
 
