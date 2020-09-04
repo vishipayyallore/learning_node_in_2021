e442b58cf07276e23f64157489c001bd47322ce1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-09-04 22:39:14 +0530

    Initial Version

---
+++ javascript-demos/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function logMethodStarting(methodName) {
+    console.log(`***** Starting ${methodName} method *****`);
+}
+
+function logMethodEnding(methodName) {
+    console.log(`===== Ending ${methodName} method =====`);
+}
