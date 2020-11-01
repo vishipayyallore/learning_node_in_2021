5d287fb39d6eb7c705c74581c14df4507f417d51
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-11-01 21:39:56 +0530

    Few Changes

---
+++ node-getting-started-js/day4/src/index.js
@@ -0,0 +1,23 @@
+console.log(`******************** Exports ********************`);
+console.log(exports);
+
+console.log(`******************** Require ********************`);
+console.log(require);
+
+console.log(`******************** Module ********************`);
+console.log(module);
+
+console.log(`******************** Filename ********************`);
+console.log(`FileName: ${__filename}`);
+
+console.log(`******************** DirName ********************`);
+console.log(`DirName: ${__dirname}`);
+
+console.log(`******************** (this === module.exports) ********************`);
+console.log(`(this === module.exports): ${this === module.exports}`);
+
+console.log(`******************** (this === global) will be true from Console ********************`);
+console.log(`(this === global): ${this === global}`);
+
+console.log(`******************** process.argv ********************`);
+process.argv.forEach(argument => console.log(argument));
