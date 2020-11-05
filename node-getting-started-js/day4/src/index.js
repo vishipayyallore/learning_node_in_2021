0634b4ffb522cc444eb731c4299b0ddcb808a2fa
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-11-05 21:41:26 +0530

    Version 1

--- node-getting-started-js/day4/src/index.js
+++ node-getting-started-js/day4/src/index.js
@@ -13,7 +13,7 @@ console.log(`FileName: ${__filename}`);
 console.log(`******************** DirName ********************`);
 console.log(`DirName: ${__dirname}`);
 
-console.log(`******************** (this === module.exports) ********************`);
+console.log(`******************** (this === module.exports) within the .js file ********************`);
 console.log(`(this === module.exports): ${this === module.exports}`);
 
 console.log(`******************** (this === global) will be true from Console ********************`);
---
+++ node-getting-started-js/day4/src/processElements.js
@@ -0,0 +1,5 @@
+
+
+console.log(`Process Object: ${JSON.stringify(process)}`);
+
+console.log(`Process Argv: ${JSON.stringify(process.argv)}`);
