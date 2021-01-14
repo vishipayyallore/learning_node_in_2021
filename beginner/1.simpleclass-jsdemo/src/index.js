c7612080028b9e6ca6098287ca6682c63e740653
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-01-14 22:16:51 +0530

    Today's program

---
+++ beginner/1.simpleclass-jsdemo/src/index.js
@@ -0,0 +1,23 @@
+/*
+    Concepts: 
+        1. Chaining Operator (?)
+*/
+const person = {
+    name: 'Shiva',
+    age: 21,
+    salary: 1234.56,
+    address: {
+        street: '101 Street',
+        city: 'Hyderabad',
+        state: 'Telangana'
+    },
+    certifications: {
+        az900: true,
+        az104: false
+    }
+};
+
+console.log(`AZ 900: ${person.certifications?.az900}`);
+console.log(`AZ 104: ${person.certifications?.az104}`);
+console.log(`AZ 204: ${person.certifications?.az204}`);
+console.log(`${JSON.stringify(person)}`);
