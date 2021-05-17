224f2090f0f7fd5629374a059036e3cc4d892b9f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-05-17 18:25:59 +0530

    Update index.ts

--- LearningAlgorithms/7increasing-sequence/src/index.ts
+++ LearningAlgorithms/7increasing-sequence/src/index.ts
@@ -1,33 +1,35 @@
-/*
-    Description: 
-*/
+// /*
+//     Description: 
+// */
 
-const increasingSequence = (values: number[]): boolean => {
-    let repeat = 0;
+import { Console } from "console";
 
-    for (let index = 0; index <= values.length && repeat <= 1; index++) {
-        if (values[index] <= values[index - 1]) {
-            repeat++;
-        }
-    }
+// const increasingSequence = (values: number[]): boolean => {
+//     let repeat = 0;
 
-    return (repeat <= 1);
-};
+//     for (let index = 0; index <= values.length && repeat <= 1; index++) {
+//         if (values[index] <= values[index - 1]) {
+//             repeat++;
+//         }
+//     }
 
-const verifyAlmostIncreasingSequence = (values: number[]) => {
-    console.log(`${values} === ${increasingSequence(values)}`);
-};
+//     return (repeat <= 1);
+// };
 
-const numbers = [
-    [1, 3, 2, 1],
-    [1, 3, 2],
-    [1, 3, 2, 4, 5],
-    [1, 3, 2, 4, 5, 3]
-];
+// const verifyAlmostIncreasingSequence = (values: number[]) => {
+//     console.log(`${values} === ${increasingSequence(values)}`);
+// };
 
-numbers.forEach((values) => {
-    verifyAlmostIncreasingSequence(values);
-});
+// const numbers = [
+//     [1, 3, 2, 1],
+//     [1, 3, 2],
+//     [1, 3, 2, 4, 5],
+//     [1, 3, 2, 4, 5, 3]
+// ];
+
+// numbers.forEach((values) => {
+//     verifyAlmostIncreasingSequence(values);
+// });
 
 
 
@@ -42,3 +44,36 @@ numbers.forEach((values) => {
 
 // values = [1, 3, 2, 4, 5, 3];
 // verifyAlmostIncreasingSequence(values);
+
+interface MyType {
+    [key: string]: boolean
+}
+
+let aList = new Array<MyType>({ ['something']: true }, { ['something2']: true });
+console.log(aList);
+
+const B: MyType = { ['something']: false };
+
+for (let index = 0; index < aList.length; index++) {
+    console.log(`${JSON.stringify(aList[index])} === ${JSON.stringify(B)}`);
+
+    for (let destinationProperty in aList[index]) {
+        for (let sourceProperty in B) {
+            if (destinationProperty === sourceProperty) {
+                aList[index] = B;
+            }
+        }
+    }
+}
+console.log(aList);
+
+// aList = aList.map(obj => 
+//     obj.key === B.key ? { ...B  } : obj
+// );
+
+
+ // const apple = JSON.parse(JSON.stringify(aList[index]));
+    // console.log(apple[0]);
+    // if (JSON.stringify(aList[index]) !== JSON.stringify(B)) {
+    //     aList[index] = B;
+    // }
